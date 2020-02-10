efe44fd779be62bf8a0560faf6411030e39be792
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-02-09 18:39:37 -0800

    added SmurfContext, SmurfCard, and get and post request

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,43 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
+import axios from "axios";
+import smurfContext from "../contexts/SmurfContext";
+
+
+function App () {
+  const [smurf, setSmurf] = useState([]);
+
+  const getSmurf = () => {
+    axios.get('http://localhost:3333/smurfs')
+      .then(res => {
+        console.log(res);
+        setSmurf(res.data);
+      })
+      .catch(err => {
+        console.log('Error!')
+      })
+  }
+
+  const postSmurf = () => {
+    axios.post('http://localhost:3333/smurfs')
+    .then(res => {
+      setSmurf(res.data)
+    })
+    .catch(err => {
+      console.log('Error!')
+    })
+  }
+  return(
+    <smurfContext.Provider value={smurf, getSmurf}>
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <h1>SMURFS! 2.0 W/ Context API</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
       </div>
-    );
-  }
+    </smurfContext.Provider>
+    
+  )
 }
 
 export default App;
